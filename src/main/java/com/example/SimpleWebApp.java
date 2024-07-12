39ea31ba79e7bdb3dbfe13ee1eca4096bb8e7e81
iuiwiff <jimin5305@naver.com>
2024-07-12 12:34:22 +0900

    테스트2

--- src/main/java/com/example/SimpleWebApp.java
+++ src/main/java/com/example/SimpleWebApp.java
@@ -15,6 +15,6 @@ public class SimpleWebApp {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, World(진짜 수정 - 테스트임!)";
+        return "Hello, World(진짜 수정 - 테스트임!!)";
     }
 }
